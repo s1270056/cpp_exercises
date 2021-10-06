4aed50decd7f59a372950f64f995fa3d19daaee2
s1270056 <69309085+s1270056@users.noreply.github.com>
2021-10-06 22:51:32 +0900

    ex01

---
+++ intStr_main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include "intStr.h"
+
+int main(){
+    int a = 12345;
+    char *b;
+    const char *c = "12345";
+    int d;
+
+    b = intToStr(a);
+    printf("%s \n",b);
+    
+    d = strToInt(c);
+    printf("%d ",d);
+    return 0;
+}
