9bb076546dc531396beb25ee94d5a6422eb23096
s1270056 <69309085+s1270056@users.noreply.github.com>
2021-11-04 17:13:43 +0900

    ex05-3 ver5

--- ex05/test_ArrayStack.cpp
+++ ex05/test_ArrayStack.cpp
@@ -14,8 +14,6 @@ int main(void)
     stack1.push(4.0);
     stack1.push(5.0);
 
-
-
     // Create another instance of ArrayStack named stack2
     //   using the other constructor, specify an original size of 5
     // Push 1.0, 2.0, ..., 10.0 in this stack
@@ -31,7 +29,6 @@ int main(void)
     stack2.push(9.0);
     stack2.push(10.0);
 
-
     // Copy
     ArrayStack stack3(stack1);
     ArrayStack stack4 = stack2;
@@ -40,14 +37,21 @@ int main(void)
     stack5 = stack1;
     ArrayStack stack6(10);
     stack6 = stack6;
-
-    stack1.pop();
-    cout << stack1.top() << endl;
-    stack3.pop();
-    cout << stack3.top() << endl;
-    stack4.pop();
-    cout << stack4.top() << endl;
-
+    while (!stack1.empty())
+    {
+        cout << stack1.top() << endl;
+        stack1.pop();
+    }
+    while (!stack3.empty())
+    {
+        cout << stack3.top() << endl;
+        stack3.pop();
+    }
+    while (!stack4.empty())
+    {
+        cout << stack4.top() << endl;
+        stack4.pop();
+    }
     // Move
     ArrayStack stack7 = std::move(stack5);
 
@@ -56,14 +60,26 @@ int main(void)
     // COMPLETE
     // Show the content of the stacks 'stack5', 'stack7', 'stack6' and 'stack2'
     // (use the same method as in question 2)
-    stack5.pop();
-    cout << stack5.top() << endl;
-    stack7.pop();
-    cout << stack7.top() << endl;
-    stack6.pop();
-    cout << stack6.top() << endl;
-    stack2.pop();
-    cout << stack2.top() << endl;
+    while (!stack5.empty())
+    {
+        cout << stack5.top() << endl;
+        stack5.pop();
+    }
+    while (!stack7.empty())
+    {
+        cout << stack7.top() << endl;
+        stack7.pop();
+    }
+    while (!stack6.empty())
+    {
+        cout << stack6.top() << endl;
+        stack6.pop();
+    }
+    while (!stack2.empty())
+    {
+        cout << stack2.top() << endl;
+        stack2.pop();
+    }
 
     return 0;
 }
