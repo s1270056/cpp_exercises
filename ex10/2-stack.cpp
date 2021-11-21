b6302c4f50188414ad1312d1d96984e96f881a2d
s1270056 <69309085+s1270056@users.noreply.github.com>
2021-11-21 19:29:38 +0900

    ex10-1

---
+++ ex10/2-stack.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <cstring>
+#include <stack>
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+double twostack(const string str){
+    double x, y;
+    char flag;
+    stringstream sstr(str);
+    stack<double> num;
+    stack<char> ope;
+    string st;
+
+    while(sstr >> st){
+
+        if(st == "+" || st == "-" || st == "*" || st == "/"){
+            flag = *st.c_str();
+            ope.push(flag);
+
+            continue;
+        }
+        else if(st == "("){
+            continue;
+           
+        }
+        else if(st == ")"){
+            if(ope.top() == '+'){
+                ope.pop();
+               
+                y = num.top();
+                num.pop();
+                x = num.top();
+                num.pop();
+                num.push(x + y);
+            }
+            else if(ope.top() == '-'){
+                ope.pop();
+                
+                y = num.top();
+                num.pop();
+                x = num.top();
+                num.pop();
+                num.push(x - y);
+            }
+            else if(ope.top() == '*'){
+                ope.pop();
+               
+                y = num.top();
+                num.pop();
+                x = num.top();
+                num.pop();
+                num.push(x * y);
+            }
+            else if(ope.top() == '/'){
+                ope.pop();
+                
+                y = num.top();
+                num.pop();
+                x = num.top();
+                num.pop();
+                num.push(x / y);
+            }
+            continue;
+
+        }
+        else{
+            num.push(stod(st));
+            continue;
+        }
+    }
+    return num.top();
+}
+
+int main(){
+    string str1 = "( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )";
+    string str2 = "( ( ( 2.0 * ( 3.0 - 1.0 ) ) - 5.0 ) * 3.0 )";
+    
+    cout << "Input: " << str1 << endl;
+    cout << "Output: "<< twostack(str1) << endl;
+
+    cout << "Input: " << str2 << endl;
+    cout << "Output: "<< twostack(str2) << endl;
+    return 0;
+
+}
