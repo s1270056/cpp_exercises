66a0fce6e1241ba5109ec7043dd89506caf4715a
s1270056 <69309085+s1270056@users.noreply.github.com>
2021-11-26 18:45:28 +0900

    ex11
    double型とchar型をついかしました。
    Assume that < is used for the comparisons.の意味がよくわかりませんでしたが、iter_swap()を使って値を交換できるようにしました。

--- ex11/pl4.cpp
+++ ex11/pl4.cpp
@@ -1,39 +1,23 @@
 #include <iostream>
 #include <vector>
+#include <iomanip>
 
 using namespace std;
 
 namespace PL4 {  
  template <class FwdIt>
  void sort(FwdIt begin, FwdIt end){
-     int tmp;
-     int tmp2;
-     int flag=0;
 
-
-     for(FwdIt i=end; i>begin; i--){
-         for(FwdIt j=begin; j<(i-1); j++){
-             flag++;    
-             tmp = *begin;  
-             //cout << tmp;
-             (begin++);     
-             //cout << *begin << endl;
-
-             if(tmp > *begin){  
-                 tmp2 = *begin; 
-                 *begin = tmp;  
-                 (begin--); 
-                 *begin = tmp2; 
-                 (begin++); 
+            for (FwdIt i = begin; i != end; ++i){
+        for (FwdIt j = begin; j < i; ++j){
+            if (*i < *j)
+                std::iter_swap(i, j);
              }
          }
-            
-             (begin -= flag);
-             flag = 0;
 
      }
  };
-}
+
 
 int main(){
     vector<int> vt;
@@ -44,19 +28,71 @@ int main(){
     vt.push_back(8);
     vt.push_back(2);
 
+        vector<double> vt2;
+    vt2.push_back(7.0);
+    vt2.push_back(6.3);
+    vt2.push_back(2.9);
+    vt2.push_back(-3);
+    vt2.push_back(9);
+    vt2.push_back(6.5);
+
+
+        vector<char> vt3;
+    vt3.push_back('a');
+    vt3.push_back('z');
+    vt3.push_back('e');
+    vt3.push_back('b');
+    vt3.push_back('n');
+    vt3.push_back('l');
+
     vector<int>::iterator it = vt.begin();
     while(it != vt.end()){
-        cout << *it;
+        cout << *it<< ", ";
         it++;
-    }
+}
+    cout << "\n";
+
+        vector<double>::iterator it2 = vt2.begin();
+    while(it2 != vt2.end()){
+        cout << *it2<< ", ";
+        it2++;
+}
+    cout << "\n";
+
+        vector<char>::iterator it3 = vt3.begin();
+    while(it3 != vt3.end()){
+        cout << *it3<< ", ";
+        it3++;
+}
     cout << "\n";
 
 
     PL4::sort(vt.begin(), vt.end());
     it = vt.begin();
     while(it != vt.end()){
-        cout << *it;
+        cout << *it<< ", ";
         it++;
+    }
+    cout << "\n";
+
+
+    cout <<  fixed << setprecision(1);
+
+
+    PL4::sort(vt2.begin(), vt2.end());
+    it2 = vt2.begin();
+    while(it2 != vt2.end()){
+        cout << *it2 << ", ";
+        it2++;
+    }
+    cout << "\n";
+
+
+    PL4::sort(vt3.begin(), vt3.end());
+    it3 = vt3.begin();
+    while(it3 != vt3.end()){
+        cout << *it3<< ", ";
+        it3++;
     }
     cout << "\n";
 
