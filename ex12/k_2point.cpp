43c84960570a80a1b5c45db6285ac8f10c21959e
s1270056 <69309085+s1270056@users.noreply.github.com>
2021-11-29 14:31:09 +0900

    ex12-2 ラムダを追加

---
+++ ex12/k_2point.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <vector>
+
+#include "k_2#include <iostream>
+#include <vector>
+
+#include "k_2point.h"
+using namespace std;
+
+int main(void) {
+    int j = 1;
+ vector<Point> points;
+
+ points.push_back(Point(1.0,2.0,3.0));
+ points.push_back(Point(1.0,1.0,1.0));
+ points.push_back(Point(4.0,2.0,1.0));
+ points.push_back(Point(9.0,2.0,1.0));
+ points.push_back(Point(1.0,2.0,1.0));
+ points.push_back(Point(0.0,0.0,1.0));
+ points.push_back(Point(5.0,0.0,0.0));
+ points.push_back(Point(2.0,2.0,1.0));
+
+ cout << "find_k_closest_2" << endl;
+ 
+ vector<Point> list = find_k_closest(4, points);
+
+ for (int i = 0; i < 4; ++i) {
+   std::cout << j << "番目に近い: " << list[i].x << ", " << list[i].y << ", " << list[i].z << std::endl;
+   j++;
+ }
+
+ cout << "find_k_closest_2" << endl;
+
+  vector<Point> list2 = find_k_closest_2(4, points);
+
+ for (int i = 0; i < 4; ++i) {
+   std::cout << j << "番目に近い: " << list2[i].x << ", " << list2[i].y << ", " << list2[i].z << std::endl;
+   j++;
+ }
+
+ return 0;
+}point.h"
+using namespace std;
+
+int main(void) {
+    int j = 1;
+ vector<Point> points;
+ points.push_back(Point(1.0,2.0,3.0));
+ points.push_back(Point(1.0,1.0,1.0));
+ points.push_back(Point(0.0,2.0,1.0));
+ points.push_back(Point(9.0,2.0,1.0));
+ points.push_back(Point(1.0,2.0,1.0));
+ points.push_back(Point(0.0,0.0,1.0));
+
+ vector<Point> closest = find_k_closest(6, points);
+
+ for (int i = 0; i < 6; ++i) {
+   std::cout << j << "番目に近い: " << closest[i].x << ", " << closest[i].y << ", " << closest[i].z << std::endl;
+   j++;
+ }
+
+ return 0;
+}
---
+++ ex12/k_2point.h
@@ -0,0 +1,86 @@
+#ifndef POINT_H
+#define POINT_H
+
+#include <iostream>
+#include <cmath>
+#include <queue>
+#include <vector>
+using namespace std;
+
+
+  struct Point {
+   Point() : x(0.0), y(0.0), z(0.0) {}
+   Point(double x, double y, double z) : x(x), y(y), z(z) {}
+   double x, y, z;
+  };
+
+  class Compare{
+      public:
+      //Compare(const Point& a, const Point& b) : a(a), b(b){}
+      
+
+      bool operator()(const Point& a, const Point& b){ 
+        auto a_sqrt = sqrt(pow(a.x, 2) + pow(a.y, 2) + pow(a.z, 2));
+        auto b_sqrt = sqrt(pow(b.x, 2) + pow(b.y, 2) + pow(b.z, 2));
+        return a_sqrt > b_sqrt;   //true or false
+        }
+
+      private:
+      Point a, b;   
+    };
+
+  vector<Point> find_k_closest(int k, vector<Point> Points)
+{
+
+
+  priority_queue<double, vector<Point>, decltype(Compare())> que{Compare()};
+
+    for(int i=Points.size()-1; i>=0; --i){
+      que.push(Points[i]);
+
+    }
+
+
+    vector<Point> list;
+
+    for(int i = 0; i<k; i++){
+        list.push_back(que.top());
+        que.pop();
+
+    }
+
+    return list;
+
+}
+
+
+
+
+ vector<Point> find_k_closest_2(int k, vector<Point> Points)
+{
+
+    auto Compare = [](const Point& a, const Point& b) -> bool{  //戻り値はbool
+      auto a_sqrt = sqrt(pow(a.x, 2) + pow(a.y, 2) + pow(a.z, 2));
+      auto b_sqrt = sqrt(pow(b.x, 2) + pow(b.y, 2) + pow(b.z, 2));
+
+      return a_sqrt > b_sqrt;   //true or false
+    };
+
+   priority_queue<double, vector<Point>, decltype(Compare)> que{Compare};
+
+    for(int i=Points.size()-1; i>=0; --i){
+      que.push(Points[i]);
+
+    }
+
+    vector<Point> list;
+    
+    for(int i = 0; i<k; i++){
+        list.push_back(que.top());
+        que.pop();
+    }
+
+    return list;
+
+}
+#endif
