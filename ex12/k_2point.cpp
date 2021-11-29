c60e9f120c02d06e3aca2b252b0e17715e60b978
s1270056 <69309085+s1270056@users.noreply.github.com>
2021-11-29 14:57:20 +0900

    ex12-2 ラムダの追加

--- ex12/k_2point.cpp
+++ ex12/k_2point.cpp
@@ -1,9 +1,6 @@
 #include <iostream>
 #include <vector>
 
-#include "k_2#include <iostream>
-#include <vector>
-
 #include "k_2point.h"
 using namespace std;
 
@@ -20,7 +17,7 @@ int main(void) {
  points.push_back(Point(5.0,0.0,0.0));
  points.push_back(Point(2.0,2.0,1.0));
 
- cout << "find_k_closest_2" << endl;
+ cout << "find_k_closest" << endl;
  
  vector<Point> list = find_k_closest(4, points);
 
@@ -35,27 +32,6 @@ int main(void) {
 
  for (int i = 0; i < 4; ++i) {
    std::cout << j << "番目に近い: " << list2[i].x << ", " << list2[i].y << ", " << list2[i].z << std::endl;
-   j++;
- }
-
- return 0;
-}point.h"
-using namespace std;
-
-int main(void) {
-    int j = 1;
- vector<Point> points;
- points.push_back(Point(1.0,2.0,3.0));
- points.push_back(Point(1.0,1.0,1.0));
- points.push_back(Point(0.0,2.0,1.0));
- points.push_back(Point(9.0,2.0,1.0));
- points.push_back(Point(1.0,2.0,1.0));
- points.push_back(Point(0.0,0.0,1.0));
-
- vector<Point> closest = find_k_closest(6, points);
-
- for (int i = 0; i < 6; ++i) {
-   std::cout << j << "番目に近い: " << closest[i].x << ", " << closest[i].y << ", " << closest[i].z << std::endl;
    j++;
  }
 
