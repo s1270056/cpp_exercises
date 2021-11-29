5ee25a96b78ac9768daed25f53b65fcebea841c6
s1270056 <69309085+s1270056@users.noreply.github.com>
2021-11-29 18:02:09 +0900

    ex13-2

---
+++ ex13/median.cpp
@@ -0,0 +1,24 @@
+#include <vectour>
+#include <iostream>
+#include <algorithm>
+#include "median.h"
+
+using namespace std;
+
+int main(void){
+
+    vector<double> num;
+    points.push_back(7.0);
+    points.push_back(14.0);
+    points.push_back(10.0);
+    points.push_back(12.0);
+    points.push_back(2.0);
+    points.push_back(11.0);
+    points.push_back(29.0);
+    points.push_back(3.0);
+    points.push_back(4.0);
+
+    cout << "Median is " << compute_median(num) << endl;
+    cout << "k: "
+
+}
---
+++ ex13/median.h
@@ -0,0 +1,20 @@
+#ifndef Median_H
+#define Median_H
+
+#include <vectour>
+#include <iostream>
+#include <algorithm>
+
+double compute_median(vector<double> num){
+
+}
+
+vector<double> compute_k_closest(int k, vector<double> num){
+    
+}
+
+
+
+
+
+#endif
