0de85b4f32230c76ab0d8416af78e7ebcf32b01f
s1270056 <69309085+s1270056@users.noreply.github.com>
2021-12-05 18:20:48 +0900

    ex03-1 修正

--- ex03/test_pointers.cpp
+++ ex03/test_pointers.cpp
@@ -1,34 +1,56 @@
 // test_pointers.cpp
 // COMPLETE include necessary headers
+#include <iostream>
+using namespace std;
 int main(void)
 {
   //
   // 1. declare a variable f as a pointer to a float
   float* f;
   // 2. create a float variable named pi, and store the value 3.14159f
-  float* pi =3.14159f;
+  float pi =3.14159f;
   // 3. make f points to pi
   f = &pi;
   // 4. print out the content of the memory location pointed to by f
-  cout << f << endl;
+  cout << *f << endl;
 
 
   //
   short a[] = {1, 2, 3, 4, 5};
   // 5. declare a variable ip as a pointer to a short integer
+  short* ip;
   // 6. make ip points to the beginning of the array a
+  ip = &a[0];
   // 7. print out what ip points to and the content of a[0] (verify they are the same)
+  cout << "ip: " << *ip << endl;
+  cout << "a[0]: " << a[0] << endl<< endl;
   // 8. increase ip by 3 and check that what it points to corresponds to a[3]
+  ip += 3;
+  cout << "ip: " << *ip << endl;
+  cout << "a[3]: " << a[3] << endl << endl;
   // 9. make ip points to the last element of the array by increasing it by 1
   //     and check that what it points to corresponds to a[4]
+  ip += 1;
+  cout << "ip: " << *ip << endl;
+  cout << "a[4]: " << a[4] << endl << endl;
 
   // 
   int n = 10;
   // 10. declare a variable d as a pointer to double and 
   // make it point to an array of "n" element of type "double" created on the heap
+  double* d;
+  d = new double[n];
   // 11. store in each d[i] (for i=0 to n-1) the value double(i) / 5.0;
+  for(int i=0; i<=n-1; i++){
+    d[i] = double(i)/5.0;
+  }
   // 12. print out each element of d
+  for(int i=0; i<=n-1; i++){
+    cout << d[i] << endl;
+  }
+  cout << endl;
   // 13. delete the previously allocated memory
+  delete []d;
 
   //
   int m = 5;
@@ -36,8 +58,19 @@ int main(void)
   double** dd;
   // 14. allocate memory for a 2d array of size m * n on the heap (i.e. m arrays of size n).  
   // Make dd points to this 2d array.
+  dd = new double*[m];
+  for(int i=0; i<m; i++){
+    dd[i] = new double[n];
+  }
   // 15. set the element dd[i][j] to be equal to double(i)*double(j)
+  for(int i=0; i<m; i++){
+    for(int j=0; j<n; j++){
+      dd[i][j] = double(i)*double(j);
+    }
+  }
   // 16. delete the memory allocated for the 2d array
+  for(int i=0; i<m; ++i) delete[]dd[i];
+  delete[]dd;
 
   return 0;
 }
