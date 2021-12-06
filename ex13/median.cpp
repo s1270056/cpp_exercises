0d5c32e6f96d2e54557ee7929e14c90c079c572e
s1270056 <69309085+s1270056@users.noreply.github.com>
2021-12-06 17:38:54 +0900

    ex13-2 修正

--- ex13/median.cpp
+++ ex13/median.cpp
@@ -1,4 +1,4 @@
-#include <vectour>
+#include <vector>
 #include <iostream>
 #include <algorithm>
 #include "median.h"
@@ -7,18 +7,34 @@ using namespace std;
 
 int main(void){
 
-    vector<double> num;
-    points.push_back(7.0);
-    points.push_back(14.0);
-    points.push_back(10.0);
-    points.push_back(12.0);
-    points.push_back(2.0);
-    points.push_back(11.0);
-    points.push_back(29.0);
-    points.push_back(3.0);
-    points.push_back(4.0);
+    int k=2;
 
-    cout << "Median is " << compute_median(num) << endl;
-    cout << "k: "
+    vector<double> num;
+    num.push_back(7.0);
+    num.push_back(14.0);
+    num.push_back(10.0);
+    num.push_back(12.0);
+    num.push_back(2.0);
+    num.push_back(11.0);
+    num.push_back(29.0);
+    num.push_back(3.0);
+    num.push_back(4.0);
+
+    double median = compute_median(num);
+
+    cout << "Median: " << median << endl;
+    cout << "k: " << k << endl;
+    
+    vector<double> close = compute_k_closest(k, num);
+
+    cout << "The k-closest: " << endl;
+
+    for (int i = 0; i < k; ++i) {
+        cout << close[i] << ", ";
+    }
+
+    cout << endl;
+
+
 
 }
--- ex13/median.h
+++ ex13/median.h
@@ -1,20 +1,52 @@
 #ifndef Median_H
 #define Median_H
 
-#include <vectour>
+#include <vector>
 #include <iostream>
 #include <algorithm>
+#include <cmath>
+
+using namespace std;
 
 double compute_median(vector<double> num){
+    size_t x = num.size()/2;
+    int size = num.size();
+    double res;
+    if(size % 2 == 0){
+        nth_element(num.begin(), num.begin() + x, num.end());
+        size_t y = x+1;
+        res = (num[x]+num[y]) / 2;
+    };
+
+    if(size % 2 == 1){
+        nth_element(num.begin(), num.begin() + x, num.end());
+        res = num[x];
+
+    };
+    return res;
 
 }
 
-vector<double> compute_k_closest(int k, vector<double> num){
-    
+
+
+vector<double> compute_k_closest(int k, const vector<double>& number){
+
+    auto mid = compute_median(number);
+    auto res = number;
+
+    auto compare = [mid](int l, int r) {
+
+        auto lef=abs(l - mid);
+        auto rig=abs(r - mid);
+
+        return lef < rig;
+
+    };
+
+    std::nth_element(res.begin(), res.begin() + number.size() / 2, res.end(), compare);
+    return res;
+
 }
-
-
-
 
 
 #endif
